87d87edb969540107dfd01ef1f212d6baf525f6a
Rocky <37962431+RockyR0ads@users.noreply.github.com>
2020-01-03 00:19:04 +1100

    version 0.5.1
    - Weight Converter now accepts decimal values
    - Added Lbs calculation to Calc percentage
    - Added stopwatch
    - New Color Scheme

--- app/src/main/java/com/example/rocky/calculatepercentage/MainActivity.java
+++ app/src/main/java/com/example/rocky/calculatepercentage/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rocky.calculatepercentage;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -47,9 +50,16 @@ public class MainActivity extends AppCompatActivity {
     TextView result;
     TextView LBresult;
 
+
     ImageView image;
 
     Switch xmlSwitch;
+
+    TextView timer ;
+    Button start, pause, reset;
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+    Handler handler;
+    int Seconds, Minutes, MilliSeconds ;
 
 
     @Override
@@ -72,12 +82,61 @@ public class MainActivity extends AppCompatActivity {
         weight2 = findViewById(R.id.ORMWeight);
         reps = findViewById(R.id.Reps);
         submit2 = findViewById(R.id.submit2);
+        timer = findViewById(R.id.tvTimer);
+        start = findViewById(R.id.btStart);
+        pause = findViewById(R.id.btPause);
+        reset = findViewById(R.id.btReset);
+
 
         df.setMaximumFractionDigits(3);
 
         image.setVisibility(View.INVISIBLE);
 
         calculateORM();
+
+        handler = new Handler() ;
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+
+                timer.setText("00:00:00");
+
+            }
+        });
 
         xmlSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -120,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if(!switchCheck) {
 
-                    poundCalc = Integer.valueOf( poundWeight.getText().toString());
+                    poundCalc = Double.valueOf( poundWeight.getText().toString());
 
                     convertedWeight = poundCalc * 2.20462;
 
@@ -135,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
 
-                    poundCalc = Integer.valueOf(poundWeight.getText().toString());
+                    poundCalc = Double.valueOf(poundWeight.getText().toString());
 
                     convertedWeight = poundCalc / 2.20462;
 
@@ -168,6 +227,7 @@ public class MainActivity extends AppCompatActivity {
 
                 // get the percentage
                 calculatedNumber = storedValue*(percentage/100.0f);
+                calculatedNumber = storedValue - calculatedNumber;
 
                 // get the weight i should be lifting
                 finalNumber = storedValue-calculatedNumber;
@@ -210,6 +270,11 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 result.setText((df.format(finalNumber)) + "Kg" + "\n");
+
+                //pound conversion
+               double poundWeight = finalNumber * 2.20462;
+                LBresult.setText((df.format((int)poundWeight)) + " Lbs" + "\n");
+                LBresult.setTextSize(50);
             }
         });
 
@@ -258,5 +323,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+
+            timer.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+
+    };
 
 }
